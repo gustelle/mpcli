6d8ac0b2d8503165259ccbfc9a58a197cefe56a9
gustelle <guillaume.patin@gmail.com>
2026-02-08 16:20:55 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mpcli
-Music Production Client
+# Music Production Client
+
+This tool provides the followins services:
+1. it enables to estimate the tempo of a set of files located in a directory
+1. it enables to convert a set of files located in a directory to a given BPM
+
+Configuration is done in the file `config.toml`
+
+Example of use case:
+* estimate the tempo of each file located in the directory
+* Take these files and convert them to 80 BPM. To do this it first detect the file source bpm. The output files are stored in a folder which is configurable
---
+++ src/mpcli/main.py
@@ -0,0 +1,123 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+import os
+import tomllib
+from pathlib import Path
+from pprint import pprint
+from typing import Annotated
+
+import audioread
+import typer
+from audiomentations import TimeStretch
+from audiomentations.core.audio_loading_utils import load_sound_file
+from audiomentations.core.transforms_interface import (
+    MultichannelAudioNotSupportedException,
+)
+from scipy.io import wavfile
+from tempocnn.classifier import TempoClassifier
+from tempocnn.feature import read_features
+from tqdm import tqdm
+
+from mpcli.entities.result import Result
+
+app = typer.Typer()
+
+
+@app.command()
+def tempo():
+
+    with open("config.toml", "rb") as f:
+        data = tomllib.load(f)
+        source_dir = data["tempo"]["source_dir"]
+
+    tempi: list[Result] = []
+
+    model_name = "cnn"
+
+    # initialize the model (may be re-used for multiple files)
+    classifier = TempoClassifier(model_name)
+
+    # read the file's features
+    for source in Path(source_dir).glob("*.wav"):
+        features = read_features(source)
+
+        # estimate the global tempo
+        tempo = classifier.estimate_tempo(features, interpolate=False)
+        tempi.append(Result(file=source.name, tempo=tempo))
+
+    pprint(tempi, indent=2)
+
+
+@app.command()
+def timestretch():
+
+    with open("config.toml", "rb") as f:
+        data = tomllib.load(f)
+        source_dir = data["timestretch"]["source_dir"]
+
+    target_tempo: float = float(data["timestretch"]["target_tempo"])
+
+    model_name = "cnn"
+
+    # initialize the model (may be re-used for multiple files)
+    classifier = TempoClassifier(model_name)
+
+    for source in Path(source_dir).glob("*.wav"):
+        try:
+            features = read_features(source)
+
+            # estimate the global tempo
+            tempo = classifier.estimate_tempo(features, interpolate=False)
+
+            # compute the time stretch factor
+            factor = target_tempo / tempo
+
+            # apply time stretch
+            transforms = [
+                {
+                    "instance": TimeStretch(
+                        # min_rate=0.86, max_rate=0.86, method="signalsmith_stretch", p=1.0
+                        min_rate=factor,
+                        max_rate=factor,
+                        method="signalsmith_stretch",
+                        p=1,  # probability of applying the transformation
+                        leave_length_unchanged=False,  # keep the output length the same as input
+                    ),
+                    "num_runs": 1,
+                    "name": "TimeStretchSignalsmithStretch",
+                },
+            ]
+
+            samples, sample_rate = load_sound_file(source, sample_rate=None, mono=False)
+            if len(samples.shape) == 2 and samples.shape[0] > samples.shape[1]:
+                samples = samples.transpose()
+
+            output_dir = data["timestretch"]["output_dir"]
+            output_dir = Path(output_dir) / str(target_tempo)
+            output_dir.mkdir(parents=True, exist_ok=True)
+
+            for transform in tqdm(transforms):
+                augmenter = transform["instance"]
+
+                output_file_path = os.path.join(
+                    output_dir,
+                    f"{source.stem}_{target_tempo}.wav",
+                )
+
+                augmented_samples = augmenter(samples=samples, sample_rate=sample_rate)
+
+                if len(augmented_samples.shape) == 2:
+                    augmented_samples = augmented_samples.transpose()
+
+                wavfile.write(
+                    output_file_path, rate=sample_rate, data=augmented_samples
+                )
+
+        except audioread.exceptions.NoBackendError as e:
+            print(f"Error processing {source}: {e}")
+        except MultichannelAudioNotSupportedException as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    app()
